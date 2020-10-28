0aacf6f3c395db7527eddee1bfef450269969239
Rania_Antar <rania.antar@esprit.tn>
2020-10-28 09:17:35 +0100

    Atelier Zuul gateway 5twin2

---
+++ src/main/java/com/esprit/microservice/GetwayeApplication.java
@@ -0,0 +1,41 @@
+package com.esprit.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import filtres.*;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class GetwayeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetwayeApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter(){
+		return new PreFilter();
+	}
+	
+	@Bean
+	public PostFilter postFilter(){
+		return new PostFilter();
+	}
+	
+	@Bean
+	public ErrorFilter errorFilter(){
+		return new ErrorFilter();
+	}
+	
+	@Bean
+	public RouteFilter routeFilter(){
+		return new RouteFilter();
+	}
+	
+
+}
